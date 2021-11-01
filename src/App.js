4a02576f8fa4638ecc94882c35e93583ddaeec96
David Souza <dabcs@poli.ufrj.br>
2021-10-31 22:18:27 -0300

    page not found

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from './Components/Home/Profile';
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import PageNotFound from './Components/404/PageNotFound';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="body-container">
-        <Profile />
-      </div>
+      <Switch>
+        <Route exact path="/" component={Profile}/>
+        <Route path="*" component={PageNotFound} />
+      </Switch>
     )
   }
 }
---
+++ src/Components/404/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PageNotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h2>404 - Página não encontrada</h2>
+      </div>
+    );
+  }
+}
