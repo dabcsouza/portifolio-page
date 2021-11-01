5f5e80f7564191fff45117edab1918b709884184
David Souza <dabcs@poli.ufrj.br>
2021-10-31 21:59:55 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom'
 export default class App extends Component {
   render() {
     return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/portifolio-page" component={Profile} />
-        <Route exact path="/" component={Profile} />
-      </Switch>
-    </BrowserRouter>
+      <div className="body-container">
+        teste
+      </div>
     )
   }
 }
